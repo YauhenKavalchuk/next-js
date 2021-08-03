6276e5731f5d04d0ca7ce170838d0f4577e801f6
YauhenKavalchuk <yauhen.kavalchuk@gmail.com>
2021-08-03 19:03:42 +0300

    Add styles. Add sass package

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Layout from "../components/layout";
-import '../styles/globals.css';
+import '../styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }) => (
   <Layout>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Heading from "../components/Heading";
+import styles from '../styles/Home.module.scss';
 
 const Home = () => (
-  <>
+  <div className={styles.wrapper}>
     <Heading text="Next.js Application" />
-  </>
+  </div>
 );
 
 export default Home;
