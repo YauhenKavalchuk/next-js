e237b948a6b557e87195a8b4f2fc020e124c3e64
YauhenKavalchuk <yauhen.kavalchuk@gmail.com>
2021-08-09 15:34:35 +0300

    Remove console

--- pages/api/socials.js
+++ pages/api/socials.js
@@ -1,7 +1,6 @@
 import { socials } from './data/socials';
 
 export default function handler(req, res) {
-  console.log(req);
   if (req.method === 'GET') {
     res.status(200).json(socials);
   };
